f4530ae7566193388831cf5b2c3181537d1e7f1e
shenyunjie <shenyunjie@aidigger.com>
2020-08-18 17:52:05 +0800

    update

--- src/custom/custom.controller.tsx
+++ src/custom/custom.controller.tsx
@@ -3,7 +3,7 @@ import { Controller, Route, Context, State, useMiddleware, useException } from "
 import { TCustomRouteData, CustomRouteData } from "./custom.interface";
 import { Template, useContextState, useContextEffect, useComponent } from '@typeclient/react';
 import { CustomTemplate } from './custom.template';
-import { CustomMiddleware, CustomMiddleware2 } from './custom.middleware';
+import { CustomMiddleware, CustomMiddleware2, CustomRedirection } from './custom.middleware';
 import { CustomError } from './custom.error';
 import { inject } from 'inversify';
 import { CustomService } from './custom.service';
@@ -40,6 +40,7 @@ export class CustomController {
         <li><span className="cur" onClick={() => ctx.redirect('/stage/6')}>错误捕获</span></li>
         <li><span className="cur" onClick={() => ctx.redirect('/stage/7')}>使用路由生命周期</span></li>
         <li><span className="cur" onClick={() => ctx.redirect('/stage/8#hash-anchor')}>Hash Anchor 锚点功能</span></li>
+        <li><span className="cur" onClick={() => ctx.redirect('/stage/9')}>中间件redirection</span></li>
       </ul>
       <h2>Repository</h2>
       <a href="https://github.com/flowxjs/TypeClient" target="_blank" rel="noopener noreferrer">View on github.</a>
@@ -122,5 +123,12 @@ export class CustomController {
       <div id="hash-anchor" style={{ width: '100%', height: '30px', lineHeight: '30px', color: 'red' }}>Hash Anchor Area! [id="hash-anchor"]</div>
       <div style={{ width: '100%', height: '10000px' }}>2. Block area!</div>
     </Fragment>
+  }
+
+  @Route('/stage/9')
+  @State(CustomRouteData)
+  @useMiddleware(CustomRedirection)
+  stage9() {
+    return <p>这是一个普通页面9</p>
   }
 }
--- src/custom/custom.middleware.ts
+++ src/custom/custom.middleware.ts
@@ -41,3 +41,11 @@ export class CustomMiddleware2<T extends Context<TCustomRouteData>> implements M
     await next();
   }
 }
+
+@Middleware()
+export class CustomRedirection<T extends Context<TCustomRouteData>> implements MiddlewareTransform<T> {
+  async use(ctx: T) {
+    await new Promise(resolve => setTimeout(resolve, 3000));
+    ctx.redirect('/stage/1');
+  }
+}
